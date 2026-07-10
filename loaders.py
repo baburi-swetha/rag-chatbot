49944efdadb234759d703e699a5a1172c54a1f76
baburi-swetha <106186055+baburi-swetha@users.noreply.github.com>
2026-07-10 13:39:54 +0530

    Update loaders.py

--- loaders.py
+++ loaders.py
@@ -2,20 +2,36 @@ import os
 import pandas as pd
 
 from docx import Document as DocxDocument
+
 from langchain_core.documents import Document
-from langchain_community.document_loaders import PyPDFLoader
+
+from docling.document_converter import DocumentConverter
+
+converter = DocumentConverter()
 
 
 def load_document(file_path):
 
     extension = os.path.splitext(file_path)[1].lower()
 
+    # -----------------------------------------
+    # PDF using Docling
+    # -----------------------------------------
     if extension == ".pdf":
 
-        loader = PyPDFLoader(file_path)
+        result = converter.convert(file_path)
 
-        return loader.load()
+    
+        text = result.document.export_to_markdown()
 
+        return [
+            Document(
+                page_content=text,
+                metadata={}
+            )
+        ]
+
+   
     elif extension == ".xlsx":
 
         df = pd.read_excel(file_path)
@@ -23,65 +39,44 @@ def load_document(file_path):
         text = df.to_string(index=False)
 
         return [
-
             Document(
-
                 page_content=text,
-
                 metadata={}
-
             )
-
         ]
 
+  
     elif extension == ".docx":
 
         doc = DocxDocument(file_path)
 
         text = "\n".join(
-
             para.text
-
             for para in doc.paragraphs
-
             if para.text.strip()
-
         )
 
         return [
-
             Document(
-
                 page_content=text,
-
                 metadata={}
-
             )
-
         ]
 
     elif extension == ".txt":
 
         with open(file_path, "r", encoding="utf-8") as f:
-
             text = f.read()
 
         return [
-
             Document(
-
                 page_content=text,
-
                 metadata={}
-
             )
-
         ]
 
     else:
 
         raise ValueError(
-
             f"Unsupported file format: {extension}"
-
         )
